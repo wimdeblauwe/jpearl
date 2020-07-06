d87384fd1f7855e2232f22a5d017202e82ac180b
Wim Deblauwe <wim.deblauwe@gmail.com>
2020-07-06 10:43:55 +0200

    Also generate unit/integration test for the repository

--- jpearl-maven-plugin/src/main/java/io/github/wimdeblauwe/jpearl/mavenplugin/GenerateEntityClassesMojo.java
+++ jpearl-maven-plugin/src/main/java/io/github/wimdeblauwe/jpearl/mavenplugin/GenerateEntityClassesMojo.java
@@ -47,6 +47,18 @@ public class GenerateEntityClassesMojo extends AbstractMojo {
     @Parameter(property = "entityIdTypeImport", defaultValue = "java.util.UUID")
     private String entityIdTypeImport;
 
+    @Parameter(property = "entityRepositoryTest")
+    private String entityRepositoryTest;
+
+    @Parameter(property = "dataJpaTestAnnotation")
+    private String dataJpaTestAnnotation;
+
+    @Parameter(property = "dataJpaTestAnnotationImport")
+    private String dataJpaTestAnnotationImport;
+
+    @Parameter(property = "entityTable")
+    private String entityTable;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         if (entityPackage == null) {
@@ -64,26 +76,55 @@ public class GenerateEntityClassesMojo extends AbstractMojo {
         if (entityRepositoryImpl == null) {
             entityRepositoryImpl = String.format("%sRepositoryImpl", entity);
         }
-
-        getLog().info(String.format("Generating entity: %s.%s", entityPackage, entity));
-        getLog().info(String.format("Generating entity id: %s.%s (using type: %s)", entityPackage, entityId, entityIdType));
-        getLog().info(String.format("Generating entity repository: %s.%s", entityPackage, entityRepository));
-        getLog().info(String.format("Generating entity repository custom: %s.%s", entityPackage, entityRepositoryCustom));
-        getLog().info(String.format("Generating entity repository impl: %s.%s", entityPackage, entityRepositoryImpl));
+        if( entityRepositoryTest == null) {
+            entityRepositoryTest = String.format("%sRepositoryTest", entity);
+        }
+        if (entityTable == null) {
+            entityTable = entity.toLowerCase();
+        }
+        if (dataJpaTestAnnotation == null) {
+            dataJpaTestAnnotation = "@DataJpaTest";
+        }
+        if (dataJpaTestAnnotationImport == null) {
+            dataJpaTestAnnotationImport = "org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest";
+        }
 
         try {
-            Path javaSourceRoot = Paths.get("src/main/java/");
-            Path entityPackageDirectory = javaSourceRoot.resolve(entityPackage.replaceAll("\\.", "/"));
-            Files.createDirectories(entityPackageDirectory);
-
-            generateFileFromTemplate(entityPackageDirectory, "entity-template.java", entity);
-            generateFileFromTemplate(entityPackageDirectory, "entityid-template.java", entityId);
-            generateFileFromTemplate(entityPackageDirectory, "entity-repository-template.java", entityRepository);
-            generateFileFromTemplate(entityPackageDirectory, "entity-repository-custom-template.java", entityRepositoryCustom);
-            generateFileFromTemplate(entityPackageDirectory, "entity-repository-impl-template.java", entityRepositoryImpl);
+            generateMainJavaCode();
+            generateTestJavaCode();
         } catch (IOException e) {
             getLog().error(e);
         }
+    }
+
+    private void generateMainJavaCode() throws IOException {
+        Path javaSourceRoot = Paths.get("src/main/java/");
+        Path entityPackageDirectory = javaSourceRoot.resolve(entityPackage.replaceAll("\\.", "/"));
+        Files.createDirectories(entityPackageDirectory);
+
+        getLog().info(String.format("Generating entity: %s.%s", entityPackage, entity));
+        generateFileFromTemplate(entityPackageDirectory, "entity-template.java", entity);
+
+        getLog().info(String.format("Generating entity id: %s.%s (using type: %s)", entityPackage, entityId, entityIdType));
+        generateFileFromTemplate(entityPackageDirectory, "entityid-template.java", entityId);
+
+        getLog().info(String.format("Generating entity repository: %s.%s", entityPackage, entityRepository));
+        generateFileFromTemplate(entityPackageDirectory, "entity-repository-template.java", entityRepository);
+
+        getLog().info(String.format("Generating entity repository custom: %s.%s", entityPackage, entityRepositoryCustom));
+        generateFileFromTemplate(entityPackageDirectory, "entity-repository-custom-template.java", entityRepositoryCustom);
+
+        getLog().info(String.format("Generating entity repository impl: %s.%s", entityPackage, entityRepositoryImpl));
+        generateFileFromTemplate(entityPackageDirectory, "entity-repository-impl-template.java", entityRepositoryImpl);
+    }
+
+    private void generateTestJavaCode() throws IOException {
+        Path javaSourceRoot = Paths.get("src/test/java/");
+        Path entityPackageDirectory = javaSourceRoot.resolve(entityPackage.replaceAll("\\.", "/"));
+        Files.createDirectories(entityPackageDirectory);
+
+        getLog().info(String.format("Generating entity repository test: %s.%s", entityPackage, entityRepositoryTest));
+        generateFileFromTemplate(entityPackageDirectory, "entity-repository-test-template.java", entityRepositoryTest);
     }
 
     private void generateFileFromTemplate(Path entityPackageDirectory, String templateName, String javaFileName) throws IOException {
@@ -98,6 +139,9 @@ public class GenerateEntityClassesMojo extends AbstractMojo {
         string = string.replaceAll("<ENTITYID>", entityId);
         string = string.replaceAll("<ENTITYIDTYPE>", entityIdType);
         string = string.replaceAll("<ENTITYIDTYPEIMPORT>", entityIdTypeImport);
+        string = string.replaceAll("<ENTITYTABLE>", entityTable);
+        string = string.replaceAll("<DATAJPATESTANNOTATION>", dataJpaTestAnnotation);
+        string = string.replaceAll("<DATAJPATESTANNOTATIONIMPORT>", dataJpaTestAnnotationImport);
         return string;
     }
 
---
+++ jpearl-maven-plugin/src/main/resources/io/github/wimdeblauwe/jpearl/mavenplugin/entity-repository-test-template.java
@@ -0,0 +1,44 @@
+package <PACKAGE>;
+
+import <DATAJPATESTANNOTATIONIMPORT>;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import <ENTITYIDTYPEIMPORT>;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+<DATAJPATESTANNOTATION>
+class <ENTITY>RepositoryTest {
+    private final <ENTITY>Repository repository;
+    private final JdbcTemplate jdbcTemplate;
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    <ENTITY>RepositoryTest(<ENTITY>Repository repository,
+                           JdbcTemplate jdbcTemplate) {
+        this.repository = repository;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @BeforeEach
+    void validatePreconditions() {
+        assertThat(repository.count()).isZero();
+    }
+
+    @Test
+    void testSave<ENTITY>() {
+        <ENTITYID> id = repository.nextId();
+        repository.save(new <ENTITY>(id));
+
+        entityManager.flush();
+
+        <ENTITYIDTYPE> idInDb = jdbcTemplate.queryForObject("SELECT id FROM <ENTITYTABLE>", <ENTITYIDTYPE>.class);
+        assertThat(idInDb).isEqualTo(id.getId());
+    }
+}
